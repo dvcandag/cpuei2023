e7481cf69a18941f585394a000ca21fca22d7c05
dvcandag <dvcandag@gmail.com>
2024-03-02 17:24:23 -0500

    Agregando avanse de DashboardMOdel

---
+++ models/DashboardModel.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'config/database.php';
+
+class DashboardModel {
+    public function getAlumnoByCodalumno($codalumno) {
+        $conn = Database::getInstance()->getConnection();
+        $query = "SELECT * FROM alumno WHERE codalumno = :codalumno";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':codalumno', $codalumno);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getNotasByCodalumno($codalumno) {
+        $conn = Database::getInstance()->getConnection();
+        $query = "SELECT * FROM nota WHERE codalumno = :codalumno";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(':codalumno', $codalumno);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
