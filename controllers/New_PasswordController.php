855b926c18daf524aa39a40e7a71e8976e3050c9
dvcandag <dvcandag@gmail.com>
2024-03-02 17:20:51 -0500

    Agregando el avance

---
+++ controllers/New_PasswordController.php
@@ -0,0 +1,28 @@
+
+<?php
+require_once '../models/New_PasswordModel.php';
+
+class New_PasswordController {
+    public function actualizarPassword() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $codalumno = $_POST["codalumno"];
+            $password = $_POST["password"];
+
+            // Validación de los datos de entrada
+            if (empty($codalumno) || empty($password)) {
+                echo "ingrese el código de alumno y la nueva contraseña.";
+                return;
+            }
+
+            $alumnoModel = new New_PasswordModel();
+            $mensaje = $alumnoModel->actualizarPassword($codalumno, $password);
+
+            echo $mensaje;
+        }
+    }
+}
+
+// Manejar la solicitud POST
+$newPasswordController = new New_PasswordController();
+$newPasswordController->actualizarPassword();
+?>
