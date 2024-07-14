a5de0db6e3beaff67aa6edc57746a71b2b696341
dvcandag <dvcandag@gmail.com>
2024-07-14 14:55:45 -0500

    Actualizando LoginController.php

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'models/LoginModel.php';
-require_once 'models/DashboardModel.php'; // Se incluye para poder acceder a los datos del alumno en la sesión
-
+require_once 'models/DashboardModel.php';
+require_once 'controllers/CursosController.php';
 
 class LoginController {
     public function showLoginForm() {
@@ -10,59 +10,37 @@ class LoginController {
 
     public function login() {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // Verifica si se reciben los datos del formulario correctamente
             $username = $_POST['codalumno'];
             $password = $_POST['password'];
 
-            // Verificar las credenciales del usuario
             $loginModel = new LoginModel();
             $user = $loginModel->getUserByUsername($username);
 
-            // Verificar si se obtiene el usuario correctamente
-            if ($user) {
-                // Obtener la contraseña almacenada en la base de datos
-                $stored_password = $user['password'];
+            if ($user && password_verify($password, $user['password'])) {
+                session_start();
+                $_SESSION["username"] = $username;
 
-                // Verificar si las contraseñas coinciden
-                if ($password === $stored_password) {
-                    // Contraseña válida, iniciar sesión
-                    session_start();
-                    $_SESSION["username"] = $username;
+                $dashboardModel = new DashboardModel();
+                $alumno = $dashboardModel->getAlumnoByCodalumno($username);
+                $_SESSION["alumno"] = $alumno;
 
+                $cursosController = new CursosController();
+                $cursosMatriculados = $cursosController->mostrarCursosMatriculados($username);
 
-
-
-// Obtener datos del alumno desde models/DashboardModel.php 
-$dashboardModel = new DashboardModel();
-$alumno = $dashboardModel->getAlumnoByCodalumno($username);
-// Guardar datos del alumno en la sesión
-$_SESSION["alumno"] = $alumno;
-
-
-
-// Obtener notas del alumno
-$dashboardModel = new DashboardModel();
-$notas = $dashboardModel->getNotasByCodalumno($alumno['codalumno']);
-
-// Guardar las notas del alumno en la sesión
-$_SESSION['notas'] = $notas;
-
-
-
-
-                    // direcciona a la pagina 
-                    header("Location: views/dashboard.php");
-                    exit;
-                } else {
-                    // Contraseña incorrecta
-                    echo "<script>alert('Credenciales incorrectas.');</script>";
-                    $this->showLoginForm(); 
+                if ($cursosMatriculados) {
+                    $_SESSION['cursosMatriculados'] = $cursosMatriculados;
                 }
-            } else {
-                // Usuario no encontrado
-                echo "<script>alert('Usuario no encontrado.');</script>";
-                $this->showLoginForm(); 
 
+                $notas = $dashboardModel->getNotasByCodalumno($alumno['codalumno']);
+                $_SESSION['notas'] = $notas;
+
+                // Redireccionar al panel de control
+                header("Location: views/dashboard.php");
+                exit;
+            } else {
+                // Credenciales incorrectas
+                echo "<script>alert('Credenciales incorrectas.');</script>";
+                $this->showLoginForm(); 
             }
         }
     }
