621c3ef568c14c1ee924ace66a2e092e5d257d07
dvcandag <dvcandag@gmail.com>
2024-07-15 19:49:10 -0500

    Actualizando views/nuevo_password.php

--- views/nuevo_password.php
+++ views/nuevo_password.php
@@ -1,30 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Actualizar Contraseña</title>
+    <!-- Enlace al archivo CSS de Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="../public/assets/css/bootstrap.css">
+    <!-- Enlace al archivo CSS de Styles -->
+    <link rel="stylesheet" type="text/css" href="../public/assets/css/styles_login.css">
 
-     <!-- Enlace al archivo CSS de Bootstrap -->
-  <link rel="stylesheet" type="text/css" href="public/assets/css/bootstrap.css">
-  <!-- Enlace al archivo CSS de Styles -->
-  <link rel="stylesheet" type="text/css" href="../public/assets/css/styles.css">
 </head>
 <body>
-
-  <div class="container-fluid" id="main-container">
-    <div class="row vh-100">
-      <!-- Formulario de inicio de sesión -->
-      <div class="col-md-12 p-4">
-        <form action="../controllers/New_PasswordController.php" method="post">
-          <h3>¿Olvidaste tu contraseña?</h3>
-          <p>Ingresa el usuario que creaste en el sistema, para poder cambiar tu contraseña.</p><br>
-          <label for="codalumno">Código de Alumno:</label>
-          <input type="text" name="codalumno" id="codalumno" placeholder="Ingresa tu código" required>
-          <label for="password">Nueva Contraseña:</label>
-          <input type="password" name="password" id="password" placeholder="Ingresa nueva contraseña" required><br><br>
-          <button type="submit" class="btn-actualizar">Actualizar contraseña</button>
-        </form>
-      </div> 
+    <div class="container">
+                <!-- Formulario de actualización de contraseña -->
+                <form action="../controllers/New_PasswordController.php" method="post">
+                    <h3 class="text-center">¿Olvidaste tu contraseña?</h3>
+                    <p class="text-center">Ingresa el usuario que utilizaste en el sistema para cambiar tu contraseña.</p></p>
+                    <div class="form-group">
+                        <label for="codalumno">Código de Alumno:</label>
+                        <input type="text" name="codalumno" id="codalumno" placeholder="Ingresa tu código" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Nueva Contraseña:</label>
+                        <input type="password" name="password" id="password" placeholder="Ingresa nueva contraseña" required>
+                    </div></p>
+                    <button type="submit" class="btn-submit">Actualizar contraseña</button>
+                </form>
+            </div>
+        </div>
     </div>
-  </div>
+
+    <!-- Enlace al archivo JS de Bootstrap -->
+    <script src=""></script>
 </body>
 </html>
