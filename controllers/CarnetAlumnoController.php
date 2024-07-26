0369971fbf7f912ac513856187d02c013b4e3700
dvcandag <dvcandag@gmail.com>
2024-07-25 21:20:06 -0500

    Cargando el controllers/CarnetAlumnoController.php

--- controllers/CarnetAlumnoController.php
+++ controllers/CarnetAlumnoController.php
@@ -2,17 +2,13 @@
 require_once 'models/CarnetAlumnoModel.php';
 
 class CarnetAlumnoController {
-    public function mostrarCarnetAlumno($codAlumno) {
-        // Crear una instancia del modelo CarnetAlumnoModel
-        $carnetAlumnoModel = new CarnetAlumnoModel();
 
-        // Obtener los datos del alumno utilizando el método del modelo
-        $alumno = $carnetAlumnoModel->obtenerDatosAlumno($codAlumno);
+    public function mostrarCarnetAlumno($codalumno) {
+        $model = new CarnetAlumnoModel();
+        $alumno = $model->obtenerAlumnoPorCodigo($codalumno);
 
         // Incluir la vista para mostrar el carnet del alumno
         include 'views/carnet-alumno.php';
     }
 }
 ?>
-
-
