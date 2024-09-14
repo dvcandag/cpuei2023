c1ffa8c897d06d471d5085c8995b92e8a6ba29a7
dvcandag <dvcandag@gmail.com>
2024-09-14 01:11:10 -0500

    Cargando la actualizacion de views/template/header.php

--- views/template/header.php
+++ views/template/header.php
@@ -26,41 +26,59 @@
                     <img src="../public/assets/images/logo/logo5.png" alt="Logo">
                 </div>
 
-                <!-- Buscador Moderno con Icono -->
-                <form class="d-none d-md-flex w-50 mx-auto formulario-busqueda">
-                    <input class="form-control rounded-pill pe-5" type="search" placeholder="Buscar..." aria-label="Search">
-                    <button class="btn" type="submit">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </form>
 
-                <!-- Links e iconos -->
-                <div class="d-flex align-items-center">
-                    <a href="#" class="nav-link">
-                        <i class="fas fa-home"></i> <!-- Icono de Home -->
-                    </a>
-                    <a href="#" class="nav-link position-relative">
-                        <i class="fas fa-envelope"></i>
-                    </a>
-                    <a href="#" class="nav-link position-relative">
-                        <i class="fas fa-comments"></i>
-                    </a>
-                    <a href="#" class="nav-link position-relative">
-                        <i class="fas fa-bell"></i>
-                    </a>
+<!-- Buscador para pantallas mediana y grande -->
+<form class="d-none d-md-flex w-50 mx-auto formulario-busqueda">
+    <input class="input-busqueda pe-5" type="search" placeholder="Busque aquí..." aria-label="Search">
+    <button class="btn" type="submit">
+        <i class="fas fa-search"></i>
+    </button>
+</form>
 
-                    <!-- Icono de búsqueda para pantallas pequeñas -->
-                    <i class="fas fa-search search-icon d-md-none" id="searchIcon"></i>
+<!-- Grupo de íconos -->
+<div class="d-flex align-items-center">
 
-                    <div class="profile-info">
-                        <div class="foto-perfil" id="profile-photo">
-                            <img src="../public/assets/images/alumnos/foto-1.jpg">
-                        </div>
-                    </div>
-                </div>
-            </div>
+<!-- Icono de búsqueda para pantallas pequeñas -->
+<div class="contenedor-icono d-block d-md-none me-2">
+    <i class="fas fa-search search-icon contenedor-icono" id="searchIcon"></i>
+</div>
+
+
+    <!-- Links e iconos -->
+    <a href="#" class="nav-link me-2">
+        <div class="contenedor-icono">
+            <i class="fas fa-home"></i> <!-- Icono de Home -->
         </div>
-    </header>
+    </a>
+    <a href="#" class="nav-link me-2 position-relative">
+        <div class="contenedor-icono">
+            <i class="fas fa-id-card"></i> <!-- Icono de Tarjeta de Identificación -->
+        </div>
+    </a>
+    <a href="#" class="nav-link me-2 position-relative">
+        <div class="contenedor-icono">
+            <i class="fas fa-envelope"></i> <!-- Icono de Enviar Correo -->
+        </div>
+    </a>
+    <a href="#" class="nav-link me-2 position-relative">
+        <div class="contenedor-icono">
+            <i class="fas fa-bell"></i> <!-- Icono de Notificaciones -->
+        </div>
+    </a>
+
+</div>
+
+
+<!-- Grupo de para foto o perfil -->
+<div class="profile-info">
+    <div class="foto-perfil" id="profile-photo">
+        <img src="../public/assets/images/alumnos/foto-1.jpg">
+    </div>
+</div>
+
+
+</div>
+ </header>
 
     <!-- Lightbox desplique del buscador en pantallas pequeñas -->
     <div class="search-lightbox" id="searchLightbox">
@@ -69,6 +87,13 @@
             <i class="fas fa-times close-lightbox" id="closeLightbox"></i>
         </div>
     </div>
+
+
+
+
+
+
+
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
