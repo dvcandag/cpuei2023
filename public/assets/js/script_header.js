057010e0c1cbbf6025d79f75bff90d43da67f3e4
dvcandag <dvcandag@gmail.com>
2024-09-17 20:17:58 -0500

    Enviando avances

--- public/assets/js/script_header.js
+++ public/assets/js/script_header.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var fotoPerfil = document.getElementById("profile-photo");
     var formularioPerfil = document.getElementById("formulario-perfil");
 
+    // Mostrar/ocultar el formulario al hacer clic en la foto del perfil
     fotoPerfil.addEventListener("click", function(event) {
         event.stopPropagation(); // Evita que el clic se propague al contenedor padre
         if (formularioPerfil.style.display === "block") {
@@ -11,12 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    // Evitar que el clic en el formulario cierre el formulario
     formularioPerfil.addEventListener("click", function(event) {
-        event.stopPropagation(); // Evita que el clic se propague al contenedor padre
+        event.stopPropagation();
     });
 
+    // Ocultar el formulario al hacer clic fuera de él
     document.addEventListener("click", function() {
-        formularioPerfil.style.display = "none";
+        if (formularioPerfil.style.display === "block") {
+            formularioPerfil.style.display = "none";
+        }
     });
 });
-
