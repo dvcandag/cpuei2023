80a0a76cc3edd794b408f9716db7d9fb117dc589
dvcandag <dvcandag@gmail.com>
2024-09-20 22:04:35 -0500

    Cargando Actualización

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -5,6 +5,11 @@ require_once 'controllers/CursosController.php';
 
 class LoginController {
     public function showLoginForm() {
+        // Si el usuario ya está autenticado, redirigir al dashboard
+        if (isset($_SESSION["username"])) {
+            header("Location: views/dashboard.php");
+            exit;
+        }
         include "views/login.php";
     }
 
@@ -16,25 +21,31 @@ class LoginController {
             $loginModel = new LoginModel();
             $user = $loginModel->getUserByUsername($username);
 
+            // Verificar que el usuario existe y la contraseña es correcta
             if ($user && password_verify($password, $user['password'])) {
                 session_start();
-                $_SESSION["username"] = $username;
 
+                // Guardar los datos del usuario en la sesión
+                $_SESSION["username"] = $username;
+                $_SESSION["nombre_completo"] = $user['nombrealumno'] . ' ' . $user['apaterno'] . ' ' . $user['amaterno']; // Nombre completo
+                
+                // Obtener los datos del alumno desde el DashboardModel
                 $dashboardModel = new DashboardModel();
                 $alumno = $dashboardModel->getAlumnoByCodalumno($username);
                 $_SESSION["alumno"] = $alumno;
 
+                // Obtener los cursos matriculados y guardarlos en la sesión
                 $cursosController = new CursosController();
                 $cursosMatriculados = $cursosController->mostrarCursosMatriculados($username);
-
                 if ($cursosMatriculados) {
                     $_SESSION['cursosMatriculados'] = $cursosMatriculados;
                 }
 
+                // Obtener las notas del alumno y guardarlas en la sesión
                 $notas = $dashboardModel->getNotasByCodalumno($alumno['codalumno']);
                 $_SESSION['notas'] = $notas;
 
-                // Redireccionar al panel de control
+                // Redirigir al panel de control (dashboard)
                 header("Location: views/dashboard.php");
                 exit;
             } else {
