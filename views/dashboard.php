cdfc4d4a4aafb8e1eb6283398c81abd2587070e8
dvcandag <dvcandag@gmail.com>
2024-10-04 21:46:27 -0500

    Cargando cambios

--- views/dashboard.php
+++ views/dashboard.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['username'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../public/assets/css/styles.css">
+    <link rel="stylesheet" href="../public/assets/css/styles_dashboard.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
@@ -25,12 +25,17 @@ if (!isset($_SESSION['username'])) {
     <?php include '../views/template/header.php'; ?>
     <?php include '../views/template/navbar.php'; ?>
 
+ <!-- Contenedor principal del Dashboard -->
+    <div class="dashboard-container">
 
-    
-    <?php include '../views/template/footer.php'; ?>
+            <?php include '../views/CursosView.php'; ?>
 
-    <h1>Bienvenido, <?php echo htmlspecialchars($_SESSION["username"]); ?></h1>
-    <a href="index.php">Cerrar Sesión</a>
+    <!-- Contenido del dashboard aquí -->
+   
+
+
+<!-- contenido del footer -->
+<?php include '../views/template/footer.php'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../public/assets/js/custom.js"></script>
