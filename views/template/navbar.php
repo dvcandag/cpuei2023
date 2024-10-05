76f20658d72b470fc6fc71953457ef8c06eca550
dvcandag <dvcandag@gmail.com>
2024-10-04 21:45:28 -0500

    enviando cambios

--- views/template/navbar.php
+++ views/template/navbar.php
@@ -1,97 +1,91 @@
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion toggled" id="accordionSidebar">
-    <!-- Sidebar - Marca -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-    </a>
 
-    <!-- Elemento de Navegación - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Tablero</span>
-        </a>
-    </li>
-
-    <!-- Encabezado -->
-    <div class="sidebar-heading">
-        Interfaz
-    </div>
-
-    <!-- Elemento de Navegación - Menú de Páginas Desplegables -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Componentes</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Componentes Personalizados:</h6>
-                <a class="collapse-item" href="buttons.html">Botones</a>
-                <a class="collapse-item" href="cards.html">Tarjetas</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Elemento de Navegación - Menú de Utilidades Desplegables -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilidades</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-bs-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Utilidades Personalizadas:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colores</a>
-                <a class="collapse-item" href="utilities-border.html">Bordes</a>
-                <a class="collapse-item" href="utilities-animation.html">Animaciones</a>
-                <a class="collapse-item" href="utilities-other.html">Otros</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Elemento de Navegación - Menú de Páginas Desplegables -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Páginas</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-bs-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Pantallas de Inicio de Sesión:</h6>
-                <a class="collapse-item" href="login.html">Iniciar Sesión</a>
-                <a class="collapse-item" href="register.html">Registrar</a>
-                <a class="collapse-item" href="forgot-password.html">Olvidé mi Contraseña</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Otras Páginas:</h6>
-                <a class="collapse-item" href="404.html">Página 404</a>
-                <a class="collapse-item" href="blank.html">Página en Blanco</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Elemento de Navegación - Gráficos -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Gráficos</span>
-        </a>
-    </li>
-
-    <!-- Elemento de Navegación - Tablas -->
-    <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tablas</span>
-        </a>
-    </li>
-
-    <!-- Alternador de Sidebar (Barra Lateral) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-
+    <!-- Incluye Font Awesome para iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
-</ul>
+    <!-- Enlace al archivo CSS de Bootstrap v5.2 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    
+    <!-- Enlace al archivo CSS de Styles -->
+    <link rel="stylesheet" href="../public/assets/css/styles_global.css">
+    <link rel="stylesheet" href="../public/assets/css/styles_navbar.css">
+
+<div class="container-fluid">
+  <div class="row flex-nowrap">
+    <div class="col-auto col-md-3 col-xl-2 px-0 navbar-container">
+      <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-clock"></i>
+              <span class="ms-1 d-none d-sm-inline">Horario</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-book"></i>
+              <span class="ms-1 d-none d-sm-inline">Cursos</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-credit-card"></i>
+              <span class="ms-1 d-none d-sm-inline">Pagos</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-graduation-cap"></i>
+              <span class="ms-1 d-none d-sm-inline">Refuerzo Académico</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-file-alt"></i>
+              <span class="ms-1 d-none d-sm-inline">Trámites</span>
+            </a>
+            </li>          
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-calendar-alt"></i>
+              <span class="ms-1 d-none d-sm-inline">Eventos</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-calendar-check"></i>
+              <span class="ms-1 d-none d-sm-inline">Citas y asesorías</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-box-open"></i>
+              <span class="ms-1 d-none d-sm-inline">Reserva de recursos</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fa-solid fa-question-circle"></i>
+              <span class="ms-1 d-none d-sm-inline">¿Necesitas ayuda?</span>
+            </a>
+          </li>
+        </ul>
+        <hr>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+     <!-- Incluye el archivo JavaScript adicional para el funcionamiento de los dropdowns -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+
+    <!-- Incluye el archivo JavaScript adicional -->
+    <script src="../public/assets/js/script_navbar.js"></script>
+
+    <!-- Incluye el archivo JavaScript de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+     <!-- Incluye el archivo JavaScript adicional -->
+    <script src="../public/assets/js/script_navegacion.js"></script>
+
