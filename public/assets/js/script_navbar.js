5344c7d51b3b90f5e31738981cfccd45c32b6768
dvcandag <dvcandag@gmail.com>
2024-10-14 22:08:32 -0500

    Entregando avance

--- public/assets/js/script_navbar.js
+++ public/assets/js/script_navbar.js
@@ -1,34 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Selecciona todos los elementos de menú desplegable
-    const dropdowns = document.querySelectorAll('.dropdown');
+// Suponiendo que tus enlaces tienen la clase 'nav-link'
+const links = document.querySelectorAll('.nav-link');
 
-    dropdowns.forEach(dropdown => {
-        const dropdownToggle = dropdown.querySelector('a');
-
-        dropdownToggle.addEventListener('click', function (event) {
-            // Evita que el clic en el botón cierre el menú inmediatamente
-            event.stopPropagation();
-
-            // Cierra todos los menús abiertos
-            dropdowns.forEach(d => {
-                if (d !== dropdown) {
-                    d.classList.remove('show');
-                }
-            });
-
-            // Alterna la clase .show en el contenedor .dropdown
-            dropdown.classList.toggle('show');
-        });
-    });
-
-    // Cerrar el menú si se hace clic fuera de él
-    document.addEventListener('click', function (event) {
-        dropdowns.forEach(dropdown => {
-            if (dropdown.classList.contains('show') && !dropdown.contains(event.target)) {
-                dropdown.classList.remove('show');
-            }
-        });
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        // Eliminar la clase 'active' de todos los enlaces
+        links.forEach(l => l.classList.remove('active'));
+        // Añadir la clase 'active' al enlace que fue clicado
+        link.classList.add('active');
     });
 });
-
-
