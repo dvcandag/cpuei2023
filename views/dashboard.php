c5c5fb1cdea87280f5d5d7a17af9655aff23bc73
dvcandag <dvcandag@gmail.com>
2024-10-15 20:10:20 -0500

    Cargando el dashboard.php

--- views/dashboard.php
+++ views/dashboard.php
@@ -3,14 +3,17 @@ session_start();
 
 // Verifica si la sesión está activa
 if (!isset($_SESSION['username'])) {
-    // Si no hay sesión activa, redirigir al login
     header("Location: index.php?action=showLoginForm");
     exit;
 }
 ?>
+<?php
+// Definir la variable $fechaActual con la fecha actual
+$fechaActual = date('d-m-Y'); // Formato de día-mes-año
+?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,24 +23,18 @@ if (!isset($_SESSION['username'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 <body>
-    
-    <!-- Aquí va el contenido del dashboard -->
     <?php include '../views/template/header.php'; ?>
     <?php include '../views/template/navbar.php'; ?>
 
- <!-- Contenedor principal del Dashboard -->
-    <div class="dashboard-container">
-
-            <?php include '../views/CursosView.php'; ?>
-
-    <!-- Contenido del dashboard aquí -->
-   
 
 
-<!-- contenido del footer -->
-<?php include '../views/template/footer.php'; ?>
+     <?php include '../views/CursosView.php'; ?>
+
+
+
+    <?php include '../views/template/footer.php'; ?>
+    <script src="../public/assets/js/slider-dashboard.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../public/assets/js/custom.js"></script>
 </body>
 </html>
