3c8d4ba9306917e3466384c659b8eb46bac9a39b
dvcandag <dvcandag@gmail.com>
2024-10-21 22:41:22 -0500

    Enviando los avances de los cambios en la navigation

--- views/template/navbar.php
+++ views/template/navbar.php
@@ -14,56 +14,56 @@
     <div class="col-auto col-md-3 col-xl-2 px-0 navbar-container">
       <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
         <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion">
               <i class="fa-solid fa-clock"></i>
               <span class="ms-1 d-none d-sm-inline">Horario</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion" data-view="CursosView">
               <i class="fa-solid fa-book"></i>
               <span class="ms-1 d-none d-sm-inline">Cursos</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion" data-view="pagos">
               <i class="fa-solid fa-credit-card"></i>
               <span class="ms-1 d-none d-sm-inline">Pagos</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion" data-view="refuerzo-academico">
               <i class="fa-solid fa-graduation-cap"></i>
               <span class="ms-1 d-none d-sm-inline">Refuerzo Académico</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion">
               <i class="fa-solid fa-file-alt"></i>
               <span class="ms-1 d-none d-sm-inline">Trámites</span>
             </a>
             </li>          
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion">
               <i class="fa-solid fa-calendar-alt"></i>
               <span class="ms-1 d-none d-sm-inline">Eventos</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion">
               <i class="fa-solid fa-calendar-check"></i>
               <span class="ms-1 d-none d-sm-inline">Citas y asesorías</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion">
               <i class="fa-solid fa-box-open"></i>
               <span class="ms-1 d-none d-sm-inline">Reserva de recursos</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
+          <li class="elemento-navegacion">
+            <a href="#" class="enlace-navegacion">
               <i class="fa-solid fa-question-circle"></i>
               <span class="ms-1 d-none d-sm-inline">¿Necesitas ayuda?</span>
             </a>
@@ -75,12 +75,14 @@
   </div>
 </div>
 
-
      <!-- Incluye el archivo JavaScript adicional para el funcionamiento de los dropdowns -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
 
     <!-- Incluye el archivo JavaScript adicional -->
     <script src="../public/assets/js/script_navbar.js"></script>
+
+    <!-- Incluye el archivo JavaScript adicional -->
+    <script src="../public/assets/js/script_navegacion.js"></script>
 
     <!-- Incluye el archivo JavaScript de Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
