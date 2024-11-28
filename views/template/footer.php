bfc91f48f4dd921238bc65f625af5339435aa33c
dvcandag <dvcandag@gmail.com>
2024-11-27 20:21:44 -0500

    Entregando los cambios requeridas

--- views/template/footer.php
+++ views/template/footer.php
@@ -3,6 +3,9 @@
 
     <link rel="stylesheet" href="../public/assets/css/styles_footer.css">
 
-<footer class="footer">
+<div class="footer" id="footer">
     <p>&copy; 2024  d´vitto. Todos los derechos reservados.</p>
 </footer>
+<!-- JS Personalizado -->
+    <script src="../public/assets/js/script_footer.js"></script>
+    
