74338fa2a9ea2b7c6f3ed8901a9a5e6ba44df053
dvcandag <dvcandag@gmail.com>
2024-12-04 21:34:30 -0500

    entregando cambios

--- models/DashboardModel.php
+++ models/DashboardModel.php
@@ -11,13 +11,14 @@ class DashboardModel {
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function getNotasByCodalumno($codalumno) {
-        $conn = Database::getInstance()->getConnection();
-        $query = "SELECT * FROM nota WHERE codalumno = :codalumno";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':codalumno', $codalumno);
-        $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
+    public function getNotasByCodalumno($codmatricula) {
+    $conn = Database::getInstance()->getConnection();
+    $query = "SELECT * FROM nota WHERE codmatricula = :codmatricula";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':codmatricula', $codmatricula);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
 }
 ?>
