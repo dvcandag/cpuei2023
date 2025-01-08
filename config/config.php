067143e406e6913893052312df25f7eceeb65726
dvcandag <dvcandag@gmail.com>
2025-01-07 19:57:55 -0500

    Cambio hecho

--- config/config.php
+++ config/config.php
@@ -1,10 +1,8 @@
-
 <?php
-define('URL', 'http://localhost/cpuei2023/');
-
-define('HOST', 'localhost');
-define('DB', 'cpuei2023');
-define('USER', 'root');
-define('PASSWORD', '');
-define('CHARSET', 'utf8mb4');
+const URL = 'http://localhost/cpuei2023/';  //carpeta del proyecto
+const HOST = 'localhost';
+const DB = 'cpuei2023';  //nombre de la base de datos
+const USER = 'root';
+const PASSWORD = '';
+const CHARSET = 'utf8mb4';
 ?>
