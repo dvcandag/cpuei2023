50aa51a0d759cfbf3a3c8fccde8e2d07ffbfa1b9
dvcandag <dvcandag@gmail.com>
2025-01-09 20:19:47 -0500

    Cargando las vistas

---
+++ views/PagosView.php
@@ -0,0 +1,51 @@
+ 
+<!-- Incluye Font Awesome para iconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+<link rel="stylesheet" type="text/css" href="../public/assets/css/styles_global.css">
+
+<link rel="stylesheet" type="text/css" href="../public/assets/css/styles_pagos.css">
+
+<div class="container-pagos">
+
+     <div class="palabra-atras">
+    <a href="dashboard.php" class="enlace-retroceso">
+        <i class="fas fa-arrow-left"></i>
+        <span class="text">Atrás</span>
+    </a>
+</div>
+        <div class="deuda">
+            <p><strong>Deuda vencida:</strong> S/. 1150.00</p>
+            <p><strong>Deuda total:</strong> S/. 1150.00</p>
+        </div>
+
+        <div class="descuento">
+            <p>10% de dcto</p>
+            <p>Paga tu ciclo completo.</p>
+            <p><a href="#">Activa este beneficio</a></p>
+            <p>Válido hasta 26 MARZO 2025</p>
+        </div>
+
+        <div class="tabla-pagos">
+            <h2>Detalles de Pagos</h2>
+            <div class="table-responsive">
+                <div class="table-header">
+                    <div class="table-cell">Item</div>
+                    <div class="table-cell">Descripción</div>
+                    <div class="table-cell">Monto</div>
+                    <div class="table-cell">Vencimiento</div>
+                    <div class="table-cell">Estado</div>
+                    <div class="table-cell">Acción</div>
+                </div>
+                <div class="table-row">
+                    <div class="table-cell">122020040001</div>
+                    <div class="table-cell">CUOTAS 01</div>
+                    <div class="table-cell">0.00</div>
+                    <div class="table-cell">18/08/23</div>
+                    <div class="table-cell">PAGADO</div>
+                    <div class="table-cell"><a href="#">Descargar</a></div>
+                </div>
+                <!-- Agrega más filas aquí según sea necesario -->
+            </div>
+        </div>
+    </div>
